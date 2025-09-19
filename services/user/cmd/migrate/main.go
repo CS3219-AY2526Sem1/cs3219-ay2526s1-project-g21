406e463ebbd4f3aa05e0a5bf5639cd91e8c81d1a
Jose <barsamic03@gmail.com>
2025-09-19 21:03:41 +0800

    chore: init boilerplate code for golang-migrate

---
+++ services/user/cmd/migrate/main.go
@@ -0,0 +1,24 @@
+package migrate
+
+import (
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+// RunMigrations applies all migrations
+func RunMigrations(databaseURL string, migrationsDir string) {
+	m, err := migrate.New("file://"+migrationsDir, databaseURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
+	}
+
+	log.Println("Migrations applied successfully!")
+}
